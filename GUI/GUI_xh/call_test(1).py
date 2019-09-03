488775a8b240054cc32d5c6130dabdd8b05712c4
lulinlin <1710684729@qq.com>
2019-09-03 11:07:42 +0800

    ver1.3 替换了显示的图片

--- GUI/GUI_xh/call_test(1).py
+++ GUI/GUI_xh/call_test(1).py
@@ -17,7 +17,7 @@ class DemoMain(QMainWindow, Ui_MainWindow):
         # self.label3 = QLabel('No Click')
         # self.tree = QTreeWidget(self)
         # self.tree1.itemClicked.connect(self.change_func)
-        pix = QPixmap(':/pic/拓扑图.png')
+        pix = QPixmap(':/pic/楼.png')
         self.label_1.setPixmap(pix)
         self.label_1.setScaledContents(True)
         self.treeWidget.itemClicked.connect(self.change_func)
@@ -30,8 +30,8 @@ class DemoMain(QMainWindow, Ui_MainWindow):
 
     def change_func(self, item, column):
 
-        self.textBrowser.setText(item.text(column))
-        self.label_2.setText(item.text(column))
+        # self.textBrowser.setText(item.text(column))
+        # self.label_2.setText(item.text(column))
         # row1 = self.tableWidget.rowCount()
         # self.tableWidget.insertRow(row1)
 
@@ -48,37 +48,46 @@ class DemoMain(QMainWindow, Ui_MainWindow):
         # self.tableWidget.verticalHeaderItem(row1).setText("节点属性")
         # self.tableWidget.removeRow(self.tableWidget.rowCount() - 1)
         topFather = self.treeWidget.currentItem()
-        while topFather.parent() is not None:
-            topFather = topFather.parent()
+        # while topFather.parent()  is not None:  #有用！！暂时注解
+        #     topFather = topFather.parent()
         # c=b.indexOfTopLevelItem(b)
         # c=b.indexOfTopLevelItem(b)
         index_father = self.treeWidget.indexOfTopLevelItem(topFather)
         # print(index_father)
         #
         # print(topFather)
+        if self.treeWidget.indexOfTopLevelItem(topFather)==-1 :
+            if item.child(0) is not None:
+                pix = QPixmap(':/pic/户.png')
+                self.label_1.setPixmap(pix)
+                self.label_1.setScaledContents(True)
         if index_father == 0:
-            pix = QPixmap(':/pic/楼1.jpg')
+            pix = QPixmap(':/pic/层1.png')
             self.label_1.setPixmap(pix)
-            self.label_1.setScaledContents(True)
+            # self.label_1.setScaledContents(True)
 
         elif index_father == 1:
-            pix = QPixmap(':/pic/楼2.jpg')
+            pix = QPixmap(':/pic/层2.png')
             self.label_1.setPixmap(pix)
-            self.label_1.setScaledContents(True)
+            # self.label_1.setScaledContents(True)
         elif index_father == 2:
-            pix = QPixmap(':/pic/楼3.jpg')
+            pix = QPixmap(':/pic/层3.png')
             self.label_1.setPixmap(pix)
-            self.label_1.setScaledContents(True)
+            # self.label_1.setScaledContents(True)
         elif index_father == 3:
-            pix = QPixmap(':/pic/楼4.jpg')
+            pix = QPixmap(':/pic/层4.png')
             self.label_1.setPixmap(pix)
-            self.label_1.setScaledContents(True)
+            # self.label_1.setScaledContents(True)
         else:
-            pix = QPixmap(':/pic/拓扑图.png')
-            self.label_1.setPixmap(pix)
-            self.label_1.setScaledContents(True)
+            # pix = QPixmap(':/pic/拓扑图.png')
+            # self.label_1.setPixmap(pix)
+            # self.label_1.setScaledContents(True)
+            pass
 
         if item.child(0) is None:  # 没有子节点意味着这是最小的孩子，直接输出该节点的信息
+            pix = QPixmap(':/pic/户.png')
+            self.label_1.setPixmap(pix)
+            self.label_1.setScaledContents(True)
             row1 = self.tableWidget.rowCount()
             num0 = 5
             if num0 < row1:  # 这里进行5个固定行的设置，多删少增
@@ -139,15 +148,15 @@ class DemoMain(QMainWindow, Ui_MainWindow):
             num3 = 0
             while num3 < num2:
                 self.tableWidget.item(num3, 0).setText(item.child(num3).text(column))  # 调整完行数之后进行输出
-                self.textBrowser.append(item.child(num3).text(column))
+                # self.textBrowser.append(item.child(num3).text(column))
                 self.tableWidget.verticalHeaderItem(num3).setText("子节点")
                 num3 = num3 + 1
 
         self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)  # 根据字体内容调整表格宽度
 
     def table_func(self):  # 暂时没用
-        self.treeWidget.reset
-        pix = QPixmap(':/pic/拓扑图.png')
+        self.treeWidget.reset()
+        pix = QPixmap(':/pic/楼.png')
         self.label_1.setPixmap(pix)
         self.label_1.setScaledContents(True)
 
